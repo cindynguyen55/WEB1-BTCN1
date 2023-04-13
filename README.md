c0e934bbdc10c4dc4f2f489d45fcc3dd4bde58bd
20120519 <lieunguyenthi1802@gmail.com>
2023-04-13 21:52:48 +0700

    done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WEB1-BTCN1
 HCMUS-3rd-year-WEB1-BTCN1
+
+
+## Nội dung
+- Bài tập cá nhân 1 môn WEB1
---
+++ script.js
@@ -0,0 +1,310 @@
+//hover menu1
+$(function () {
+  $("#tmenu1").hover(
+    function (e) {
+      $("#tmenu1").css("color", "rgb(255, 216, 177)");
+      $("#tmenu1").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu1").css("color", "rgb(255, 216, 177)");
+      $("#bmenu1").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu1").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu1").css("color", "rgb(59, 132, 232)");
+      $("#tmenu1").css("text-shadow", "none");
+      $("#bmenu1").css("color", "rgb(255, 130, 7)");
+      $("#bmenu1").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu1").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+$(function () {
+  $("#bmenu1").hover(
+    function (e) {
+      $("#tmenu1").css("color", "rgb(255, 216, 177)");
+      $("#tmenu1").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu1").css("color", "rgb(255, 216, 177)");
+      $("#bmenu1").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu1").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu1").css("color", "rgb(59, 132, 232)");
+      $("#tmenu1").css("text-shadow", "none");
+      $("#bmenu1").css("color", "rgb(255, 130, 7)");
+      $("#bmenu1").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu1").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+//hover menu2
+$(function () {
+  $("#tmenu2").hover(
+    function (e) {
+      $("#tmenu2").css("color", "rgb(255, 216, 177)");
+      $("#tmenu2").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu2").css("color", "rgb(255, 216, 177)");
+      $("#bmenu2").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu2").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu2").css("color", "rgb(59, 132, 232)");
+      $("#tmenu2").css("text-shadow", "none");
+      $("#bmenu2").css("color", "rgb(255, 130, 7)");
+      $("#bmenu2").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu2").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+$(function () {
+  $("#bmenu2").hover(
+    function (e) {
+      $("#tmenu2").css("color", "rgb(255, 216, 177)");
+      $("#tmenu2").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu2").css("color", "rgb(255, 216, 177)");
+      $("#bmenu2").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu2").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu2").css("color", "rgb(59, 132, 232)");
+      $("#tmenu2").css("text-shadow", "none");
+      $("#bmenu2").css("color", "rgb(255, 130, 7)");
+      $("#bmenu2").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu2").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+//hover menu3
+$(function () {
+  $("#tmenu3").hover(
+    function (e) {
+      $("#tmenu3").css("color", "rgb(255, 216, 177)");
+      $("#tmenu3").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu3").css("color", "rgb(255, 216, 177)");
+      $("#bmenu3").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu3").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu3").css("color", "rgb(59, 132, 232)");
+      $("#tmenu3").css("text-shadow", "none");
+      $("#bmenu3").css("color", "rgb(255, 130, 7)");
+      $("#bmenu3").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu3").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+$(function () {
+  $("#bmenu3").hover(
+    function (e) {
+      $("#tmenu3").css("color", "rgb(255, 216, 177)");
+      $("#tmenu3").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu3").css("color", "rgb(255, 216, 177)");
+      $("#bmenu3").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu3").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu3").css("color", "rgb(59, 132, 232)");
+      $("#tmenu3").css("text-shadow", "none");
+      $("#bmenu3").css("color", "rgb(255, 130, 7)");
+      $("#bmenu3").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu3").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+//hover menu4
+$(function () {
+  $("#tmenu4").hover(
+    function (e) {
+      $("#tmenu4").css("color", "rgb(255, 216, 177)");
+      $("#tmenu4").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu4").css("color", "rgb(255, 216, 177)");
+      $("#bmenu4").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu4").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu4").css("color", "rgb(59, 132, 232)");
+      $("#tmenu4").css("text-shadow", "none");
+      $("#bmenu4").css("color", "rgb(255, 130, 7)");
+      $("#bmenu4").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu4").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+$(function () {
+  $("#bmenu4").hover(
+    function (e) {
+      $("#tmenu4").css("color", "rgb(255, 216, 177)");
+      $("#tmenu4").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu4").css("color", "rgb(255, 216, 177)");
+      $("#bmenu4").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu4").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu4").css("color", "rgb(59, 132, 232)");
+      $("#tmenu4").css("text-shadow", "none");
+      $("#bmenu4").css("color", "rgb(255, 130, 7)");
+      $("#bmenu4").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu4").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+//hover menu5
+$(function () {
+  $("#tmenu5").hover(
+    function (e) {
+      $("#tmenu5").css("color", "rgb(255, 216, 177)");
+      $("#tmenu5").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu5").css("color", "rgb(255, 216, 177)");
+      $("#bmenu5").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu5").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu5").css("color", "rgb(59, 132, 232)");
+      $("#tmenu5").css("text-shadow", "none");
+      $("#bmenu5").css("color", "rgb(255, 130, 7)");
+      $("#bmenu5").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu5").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+$(function () {
+  $("#bmenu5").hover(
+    function (e) {
+      $("#tmenu5").css("color", "rgb(255, 216, 177)");
+      $("#tmenu5").css("text-shadow", "1px 1px 3px rgb(255, 130, 7)");
+      $("#bmenu5").css("color", "rgb(255, 216, 177)");
+      $("#bmenu5").css("text-decoration", "underline rgb(255, 216, 177) 1px");
+      $("#bmenu5").css("background-color", "rgb(255, 130, 7)");
+    },
+    function (e) {
+      $("#tmenu5").css("color", "rgb(59, 132, 232)");
+      $("#tmenu5").css("text-shadow", "none");
+      $("#bmenu5").css("color", "rgb(255, 130, 7)");
+      $("#bmenu5").css("text-decoration", "underline rgb(255, 130, 7) 1px");
+      $("#bmenu5").css("background-color", "rgb(255, 216, 177)");
+    }
+  );
+});
+//validation phần dữ liệu nhập vào
+function OnDangky(event) {
+  if (checkID($("#mssv").val()) == false) {
+    $("#mssv").css("color", "red");
+  } else {
+    $("#mssv").css("color", "black");
+  }
+  if (checkName($("#hoten").val()) == false) {
+    $("#hoten").css("color", "red");
+  } else {
+    $("#hoten").css("color", "black");
+  }
+  if (checkDC($("#diachi").val()) == false) {
+    $("#diachi").css("color", "red");
+  } else {
+    $("#diachi").css("color", "black");
+  }
+  if (checkPhonenumber($("#dienthoai").val()) == false) {
+    $("#dienthoai").css("color", "red");
+  } else {
+    $("#dienthoai").css("color", "black");
+  }
+  if (checkEmail($("#email").val()) == false) {
+    $("#email").css("color", "red");
+  } else {
+    $("#email").css("color", "black");
+  }
+}
+function checkID(str) {
+  let regex1 = /^\d{8}$/;
+  var regex2 = str.localeCompare("16") == 1 && str.localeCompare("23") == -1;
+  return regex1.test(str) && regex2;
+}
+function checkName(str) {
+  let regex1 = /^[A-Z].{1,}(\s[A-Z].{1,}){1,}$/;
+  return regex1.test(str);
+}
+function checkDC(str) {
+  let regex1 = /^\w.{1,}(\s\w.{1,}){1,}$/;
+  return regex1.test(str);
+}
+function checkEmail(str) {
+  let regex = /^[a-z][a-z0-9_\.]{5,29}@[a-z0-9]{2,}(\.[a-z0-9]{2,}){1,3}$/;
+  return regex.test(str);
+}
+function checkPhonenumber(str) {
+  let regex = /^[0]\d{9}$/;
+  return regex.test(str);
+}
+//đăng ký 1 nhóm môn
+function dkmonhoc(e) {
+  var dsmon = document.getElementsByClassName("dsmonactive");
+  var them = document.getElementById("uldsmondc");
+  i=0;
+  while(dsmon.length!=0)
+  {
+    var p = document.createElement("li");
+    p.interHTML = '<li class="dsmondc">' + dsmon[i].innerText + "</li>";
+    p.innerText = dsmon[i].innerText;
+    p.className = "dsmondc";
+    p.addEventListener("click", function () {
+      this.classList.toggle("dsmondcactive");
+    });
+    dsmon[i].hidden = true;
+    dsmon[i].className = null;
+    them.appendChild(p);
+  }
+}
+//huỷ đăng ký 1 nhóm môn
+function huydkmonhoc(e) {
+  var dsmon = document.getElementsByClassName("dsmondcactive");
+  var them = document.getElementById("uldsmon");
+  i=0;
+  while(dsmon.length!=0)
+  {
+    var p = document.createElement("li");
+    p.interHTML = '<li class="dsmon">' + dsmon[i].innerText + "</li>";
+    p.innerText = dsmon[i].innerText;
+    p.className = "dsmon";
+    p.addEventListener("click", function () {
+      this.classList.toggle("dsmonactive");
+    });
+    dsmon[i].hidden = true;
+    dsmon[i].className = null;
+    them.appendChild(p);
+  }
+}
+//đăng ký tất cả nhóm môn
+function dkmonhoc_all(e) {
+  var dsmon = document.getElementsByClassName("dsmon");
+  var them = document.getElementById("uldsmondc");
+  i=0;
+  while(dsmon.length!=0)
+  {
+    var p = document.createElement("li");
+    p.interHTML = '<li class="dsmondc">' + dsmon[i].innerText + "</li>";
+    p.innerText = dsmon[i].innerText;
+    p.className = "dsmondc";
+    p.addEventListener("click", function () {
+      this.classList.toggle("dsmondcactive");
+    });
+    dsmon[i].hidden = true;
+    dsmon[i].className = null;
+    them.appendChild(p);
+  }
+}
+//huỷ đăng ký tất nhóm môn
+function huydkmonhoc_all(e) {
+  var dsmon = document.getElementsByClassName("dsmondc");
+  var them = document.getElementById("uldsmon");
+  i=0;
+  while(dsmon.length!=0)
+  {
+    var p = document.createElement("li");
+    p.interHTML = '<li class="dsmon">' + dsmon[i].innerText + "</li>";
+    p.innerText = dsmon[i].innerText;
+    p.className = "dsmon";
+    p.addEventListener("click", function () {
+      this.classList.toggle("dsmonactive");
+    });
+    dsmon[i].hidden = true;
+    dsmon[i].className = null;
+    them.appendChild(p);
+  }
+}
